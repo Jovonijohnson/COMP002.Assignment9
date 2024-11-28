3d5aeaff7b6ea450071af7bd60eac1be4a2c8f50
Jovonijohnson <Jovenom25@yahoo.com>
2024-11-28 05:45:10 -0800

    added answers to code

--- script.js
+++ script.js
@@ -4,18 +4,21 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
+console.log("Utilizing function techniques and repetition were effective. Also, engaging in discussions and projects helped reinforce my understanding of the material.");
 console.log("");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
+console.log("It would have been beneficial to have more real-world case studies and examples integrated into the curriculum. Additionally, more frequent feedback on assignments could enhance learning and help identify areas needing improvement sooner.");
 console.log("");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
+console.log("For the most part, yes. The course covered the key concepts I anticipated, and I also discovered some unexpected but valuable insights. However, there were a few topics I hoped to delve deeper into.");
 console.log("");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
@@ -24,4 +27,5 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
+console.log("Advanced algorithms and the intricacies of calling functions were new to me. Familiar concepts and basic programming principles is what I encountered already.");
 console.log("");
